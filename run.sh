dfe317b8b94e9d0f03912b789b7bbce2674181db
alireza <140397454+deadhackers@users.noreply.github.com>
2023-07-24 10:31:45 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,6 @@
 clear
 
 banner() {
-printf "\n\e[1;92m"
-printf "\n\e[1     ▐██▄░▐██▌░▐█░░███▄░▐▌░█░░▐█░░▄██▌▐▌▄█░███▄███░███
-                   ▐▌▀█▄▐▌░░░██▄░█░▀█░▐▌░█░░██▄░█▌░▀▐▌█▌░█░░▐█░█░█░▀
-                   ▐▌░▐▌▐▌░░░█▐▌░█░░█▌▐█▄█░░█▐▌░█░░░▐██░░█░░▐█░█░█▄
-                   ▐▌░▐▌▐██░▄▌▀█░█░░▐▌▐███░▐▌▀█░█░░░▐██░░██▌▐███░▀██
-                   ▐▌░▐▌▐▌░░▐█▄█░█░░█▌▐▌░█░▐█▄█░█░░░▐▌█▄░█░░▐█▀█░░░█
-                   ▐█▄█▀▐█▄▄█▀▀█▄█▄▄█░▐▌░█░█▀▀█▄██░▄▐▌▐█░█▄▄▐█░█░▄▄█
-                   ▐██▀░▐██▌█░░▐▌███▀░▐▀░█░█░░▐▌▀██▀▐▌░█░███▀█░▐▌██▀  \e[1;95mv1.8-BETA\n"
-
-
-                          
 printf "\n\e[1;92m   telegram : \e[1;94m@deadhacker1\n\n\e[1;92m"
 }
 dependencies() {
@@ -21,8 +10,8 @@ command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... 
 command -v python3 > /dev/null 2>&1 || { echo >&2 "Package Python3 is not installed ... Aborting ..."; exit 1; }
 }
 script() {
-printf "\n\e[1;92m[\e[1;93m01\e[1;92m] Find information via phone number\n[\e[1;93m02\e[1;92m] Find location via IP Address/Website Link\n[\e[1;93m03\e[1;92m] Phone Number Tracker (Recommanded to find IP Address)\n[\e[1;93m04\e[1;92m] Exit"
-read -p $'\n\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m]\e[1;93m Choose an option: \e[1;93m' option
+printf "\n\e[1;92m[\e[1;93m01\e[1;92m]  phone number\n[\e[1;93m02\e[1;92m]  IP Address/Website Link\n[\e[1;93m03\e[1;92m] Phone Number Tracker \n[\e[1;93m04\e[1;92m] Exit"
+read -p $'\n\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m]\e[1;93m  option: \e[1;93m' option
 
 if [[ $option == 1 || $option == 01 ]]; then
 	printf "\n\nEnter your phone number in international format ...\n"
