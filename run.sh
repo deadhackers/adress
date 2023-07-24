59d80e7e6e84d90fda879849fd91b6a99e0e4020
alireza <140397454+deadhackers@users.noreply.github.com>
2023-07-24 10:55:53 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ if [[ $option == 1 || $option == 01 ]]; then
 	printf "\n\e[1;92m This process will take a few moments ...\n\n\e[1;92m"
 	sleep 2.9
 	python3 files/phoneinfoga.py -n $optiona
-	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttp://t.me/linux_repo\e[1;92m\n"
+	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttps://t.me/Deadhacker1\e[1;92m\n"
 	script
 
 elif [[ $option == 2 || $option == 02 ]]; then
@@ -30,7 +30,7 @@ elif [[ $option == 2 || $option == 02 ]]; then
 	printf "\n\e[1;92m Please wait ...\n\n\e[1;92m"
 	sleep 3
 	python3 files/ipgeolocation.py -t $optionb
-	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttp://t.me/linux_repo\e[1;92m\n"
+	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttps://t.me/Deadhacker1\e[1;92m\n"
 	script
 
 elif [[ $option == 3 || $option == 03 ]]; then
@@ -38,16 +38,16 @@ elif [[ $option == 3 || $option == 03 ]]; then
 	read -p $'\n\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m]\e[1;93m Enter Number: \e[1;92m' optionc
 
 	python3 files/tracker.py $optionc
-	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttp://t.me/linux_repo\e[1;92m\n"
+	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttps://t.me/Deadhacker1\e[1;92m\n"
 	script
 
 elif [[ $option == 4 || $option == 04 ]]; then
-	printf "\n\n\e[1;92m Join Telegram Group for feedback:\e[1;96m http://t.me/linux_repo\e[1;92m\n"
+	printf "\n\n\e[1;92m Join Telegram Group for feedback:\e[1;96m https://t.me/Deadhacker1\e[1;92m\n"
 	exit 1
 
 else
 	printf "\n\n\e[1;91mCommand not found!\e[1;92m\n"
-	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttp://t.me/linux_repo\e[1;92m\n"
+	printf "\n\n\e[1;92mNOTE: If you get any problem while using this tool than please report to\nTelegram Group: \e[1;96mhttps://t.me/Deadhacker1\e[1;92m\n"
 
 fi
 }
